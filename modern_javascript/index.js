ae941a442d8388408572a4884b71ddba3dc52742
melaniegrigg <melanie.grigger@gmail.com>
2020-07-30 20:43:00 -0400

    initial commit

---
+++ modern_javascript/index.js
@@ -0,0 +1,24 @@
+console.log('hello from javascript');
+console.log(moment("20141031", "YYYYMMDD").fromNow());
+
+// function oneYearAgo(moment) {
+//   var moment = moment().format("MMM Do YY"); {
+//     if (moment === "Jul 30th 20");
+//     console.log("That is today!");
+//     else
+//     console.log(moment().subtract(324, 'days').calendar());
+//   }
+
+// }
+
+// console.log(oneYearAgo(moment));
+
+// if (x > 50) {
+//   console.log("High number");
+// } else if (x < 25) {
+//   console.log("Low number");
+// } else if (x === 31) {
+//   console.log("My favorite number");
+// } else {
+//   console.log("Medium number");
+// }
